ac4a522c499e8fb6d8cf2e02afba8f5f73de34cf
Ermi 13 <ermides890@gmail.com>
2024-04-10 13:12:38 +0300

    Updated fixes

--- script.js
+++ script.js
@@ -12,43 +12,61 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const blurElements = document.querySelectorAll(" .About_me ,.contact");
 
-  const closeBtn = document.querySelector(".close-btn");
-  closeBtn.style.display = "none";
+  // const closeBtn = document.querySelector(".close-btn");
+  // closeBtn.style.display = "none";
 
-  menuBtn.addEventListener("click", function () {
+  // menuBtn.addEventListener("click", function () {
+  //   mainNav.classList.toggle("show");
+  //   blurElements.forEach((element) => {
+  //     element.classList.add("overlay");
+  //   });
+  //   menuBtn.style.display = "none";
+  //   closeBtn.style.display = "block";
+  // });
+
+  // // event listener to the close button for toggling the menu and blur effect
+  // closeBtn.addEventListener("click", function () {
+  //   mainNav.classList.toggle("show");
+  //   blurElements.forEach((element) => {
+  //     element.classList.remove("overlay");
+  //   });
+
+  //   menuBtn.style.display = "block";
+  //   closeBtn.style.display = "none";
+  // });
+
+  document.querySelector(".menu-button").addEventListener("click", function () {
     mainNav.classList.toggle("show");
     blurElements.forEach((element) => {
-      element.classList.add("overlay");
+      element.classList.toggle("overlay");
     });
-    menuBtn.style.display = "none";
-    closeBtn.style.display = "block";
+    document.querySelector(".menu-open").classList.toggle("hidden");
+    document.querySelector(".menu-close").classList.toggle("hidden");
   });
 
-  // event listener to the close button for toggling the menu and blur effect
-  closeBtn.addEventListener("click", function () {
-    mainNav.classList.toggle("show");
-    blurElements.forEach((element) => {
-      element.classList.remove("overlay");
-    });
-    menuBtn.style.display = "block";
-    closeBtn.style.display = "none";
-  });
   navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
-
+      // if (
+      //   (
+      //     window.matchMedia("(max-width: 768px)") ||
+      //     window.matchMedia("(max-width: 650px)")
+      //   ).matches
+      // ) {
+      //   menuBtn.style.display = "block";
+      //   closeBtn.style.display = "none";
+      // }
       navLinks.forEach((navLink) => {
         navLink.classList.remove("active");
       });
       link.classList.add("active");
 
       mainNav.classList.remove("show");
+      document.querySelector(".menu-open").classList.remove("hidden");
+      document.querySelector(".menu-close").classList.add("hidden");
+
       blurElements.forEach((element) => {
         element.classList.remove("overlay");
-        if (window.matchMedia("(max-width: 768px)").matches) {
-          menuBtn.style.display = "block";
-          closeBtn.style.display = "none";
-        }
       });
       // menuBtn.style.display = "block";
       // closeBtn.style.display = "none";
@@ -62,9 +80,16 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+  // if (
+  //   !window.matchMedia("(max-width: 768px)").matches ||
+  //   !window.matchMedia("(max-width: 768px)").matches
+  // ) {
+  //   menuBtn.style.display = "block";
+  //   closeBtn.style.display = "none";
+  // }
 });
 
-// Add event listener to the submit button of the contact form
+// Event listener to the submit button of the contact form
 const submitButton = document.querySelector(".send");
 submitButton.addEventListener("click", function (e) {
   e.preventDefault();
@@ -76,7 +101,7 @@ submitButton.addEventListener("click", function (e) {
   );
   const messageTextarea = document.querySelector("textarea");
 
-  // Validate the form fields
+  // Validating the form fields
   if (
     nameInput.value.trim() === "" ||
     emailInput.value.trim() === "" ||
@@ -86,7 +111,7 @@ submitButton.addEventListener("click", function (e) {
     return;
   }
 
-  // Validate the email format
+  // Validating the email format
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailPattern.test(emailInput.value.trim())) {
     alert("Please enter a valid email address.");
